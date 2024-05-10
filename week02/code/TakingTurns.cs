a103912baad804c87708c665fd4ef22c8dc4e622
cprincec <chukwuprincec@gmail.com>
2024-05-10 15:03:16 +0100

    completed Taking Turns program

--- week02/code/TakingTurns.cs
+++ week02/code/TakingTurns.cs
@@ -17,12 +17,11 @@
         Console.WriteLine("My print out", players);    // This can be un-commented out for debug help
         while (players.Length > 0)
             players.GetNextPerson();
-        // Defect(s) Found: Each player is printed out for each availbele turn before a new player is printed
-        // instead of having printing the names of players in the queue once then the next player and so on till
+        // Defect(s) Found: Each player is printed out for each availbele turn before a new/next player is printed
+        // instead of printing the names of players in the queue once then the next player and so on till
         // a player's turns are exhausted.
-        // Also the person's in the queue are printed based on last in first out. That is the last person added to the queue
-        // is printed till the turns are exhausted before the next to the last person is printed until hist turns ae exhausted and the cycle continues
-
+        // The Enqueue method adds new persons to the front (index 0) of the personQueue list instead of at the
+        // back (last index + 1) of the list.
 
         Console.WriteLine("---------");
 
@@ -46,7 +45,9 @@
         while (players.Length > 0)
             players.GetNextPerson();
 
-        // Defect(s) Found: 
+        // Defect(s) Found: A player is printed until his turn is exhausted  or when the for loop that
+        // calls the GetNextPerson() method has run five times before the next player/turn is printed. 
+
 
         Console.WriteLine("---------");
 
@@ -65,7 +66,10 @@
             players.GetNextPerson();
             // Console.WriteLine(players);
         }
-        // Defect(s) Found: 
+        // Defect(s) Found: A player's turn is exhausted before before the next player is printed. Also, 
+        // Adding a player with 0 turns only enqueues the player once; so player's added with 0 turns are printed once instead
+        // of infinity or the number of times indicated by the loop.
+        // I got the 'No one in the queue' error even when there is a player with infinity turns in the queue.
 
         Console.WriteLine("---------");
 
@@ -83,7 +87,8 @@
             players.GetNextPerson();
             // Console.WriteLine(players);
         }
-        // Defect(s) Found: 
+        // Defect(s) Found: Player with infinity (-3) turns is only enqueued/printed once, also players are not printed in turns by each player's
+        // is printed until his turns are exhausted before the next player is printed.
 
         Console.WriteLine("---------");
 
@@ -93,6 +98,13 @@
         Console.WriteLine("Test 5");
         players = new TakingTurnsQueue();
         players.GetNextPerson();
-        // Defect(s) Found:
+        // Defect(s) Found: No defect was found as the proper error message of 'No one in the queue' was displayed
+        // indicating that the queue is empty.
     }
 }
+
+
+
+
+
+
